a51b234eecbace6f8e0b7f80e05993767eab7b27
rvianapy <roviol@gmail.com>
2018-12-30 15:04:50 -0200

    Inserção de testes condicionais para validar peso e altura

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -2,12 +2,29 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 var paciente = document.querySelector("#primeiro-paciente");
-
+var tdIMC = paciente.querySelector(".info-imc");
 var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
-
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
+var alturaEhValida = true;
+var pesoEhValido = true;
 
-var imc = peso / (altura * altura);
-console.log(imc);
+if (peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdIMC.textContent = "Peso inválido!";
+}
+
+if (altura <= 0 || altura >= 3.00) {
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+    tdIMC.textContent = "Altura inválida!";
+}
+
+if (pesoEhValido && alturaEhValida) {
+    var imc = peso / (altura * altura);
+    tdIMC.textContent = imc;
+} else {
+    tdIMC.textContent = "Altura e/ou peso inválidos!";
+}
