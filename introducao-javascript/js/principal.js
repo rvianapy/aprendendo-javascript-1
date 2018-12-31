78a19b3b3b44e72b1f26c611b02b5dc15b6e8a14
rvianapy <roviol@gmail.com>
2018-12-31 11:01:37 -0200

    Replicando a validação e o cálculo do IMC para todos os pacientes

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,30 +1,35 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdIMC = paciente.querySelector(".info-imc");
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
-var alturaEhValida = true;
-var pesoEhValido = true;
+pacientes = document.querySelectorAll(".paciente");
 
-if (peso <= 0 || peso >= 1000) {
-    console.log("Peso inválido!");
-    pesoEhValido = false;
-    tdIMC.textContent = "Peso inválido!";
-}
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
 
-if (altura <= 0 || altura >= 3.00) {
-    console.log("Altura inválida!");
-    alturaEhValida = false;
-    tdIMC.textContent = "Altura inválida!";
-}
+    var tdIMC = paciente.querySelector(".info-imc");
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    var alturaEhValida = true;
+    var pesoEhValido = true;
 
-if (pesoEhValido && alturaEhValida) {
-    var imc = peso / (altura * altura);
-    tdIMC.textContent = imc;
-} else {
-    tdIMC.textContent = "Altura e/ou peso inválidos!";
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdIMC.textContent = "Peso inválido!";
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdIMC.textContent = "Altura inválida!";
+    }
+
+    if (pesoEhValido && alturaEhValida) {
+        var imc = peso / (altura * altura);
+        tdIMC.textContent = imc.toFixed(2);
+    } else {
+        tdIMC.textContent = "Altura e/ou peso inválidos!";
+    }
 }
