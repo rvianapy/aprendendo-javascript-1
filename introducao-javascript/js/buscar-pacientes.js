96d3b7d714d0e5ad125d20b3cf5b7d0b247c6a4e
rvianapy <roviol@gmail.com>
2019-01-05 20:55:17 -0200

    Tratamento de erros de requisição.

--- introducao-javascript/js/buscar-pacientes.js
+++ introducao-javascript/js/buscar-pacientes.js
@@ -6,12 +6,20 @@ botaoBuscar.addEventListener("click", function() {
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
     xhr.addEventListener("load", function() {
-        var resposta = xhr.responseText;
-        var pacientes = JSON.parse(resposta);
+        var erroAjax = document.querySelector("#erro-ajax");
 
-        pacientes.forEach( function(paciente) {
-            adicionaPacienteNaTabela(paciente);
-        });
+        if (xhr.status == 200) {
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+
+            pacientes.forEach( function(paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            erroAjax.classList.remove("invisivel");
+        }
+
     });
 
     xhr.send();
